743bbc81a922397ff63d3284c633365cd8c63004
Haydar Algün <haydar.alguen@gmail.com>
2024-03-05 12:04:34 +0100

    solved js dark mode challange and js survex error challange

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkModeBtn = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeBtn = document.querySelector('[data-js="light-mode-button"]');
+const toggleModeBtn = document.querySelector('[data-js="toggle-button"]');
+
+darkModeBtn.addEventListener("click", ()=>{
+    bodyElement.classList.add("dark");
+});
+
+lightModeBtn.addEventListener("click", ()=>{
+    bodyElement.classList.remove("dark");
+});
+
+toggleModeBtn.addEventListener("click", ()=>{
+    bodyElement.classList.toggle("dark");
+});
